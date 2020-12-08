47825c0842fa54e438107f452a5854f1c473a222
Vincent ABRIOU <vincent.abriou@st.com>
2020-12-08 10:04:20 +0100

    update of licenses
    Signed-off-by: Vincent ABRIOU <vincent.abriou@st.com>
    Change-Id: I613474df6e59fae02cbafec62c56e6d483a59304
    Reviewed-on: https://gerrit.st.com/c/stm32mpuapp/meta/meta-st-stm32mpu-ai/+/187381

--- recipes-samples/tflite-edgetpu-benchmark/files/wrapper_tfl_edgetpu.hpp
+++ recipes-samples/tflite-edgetpu-benchmark/files/wrapper_tfl_edgetpu.hpp
@@ -1,17 +1,26 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+/*
+ * wrapper_tfl_edgetpu.hpp
+ *
+ * Author: Vincent Abriou <vincent.abriou@st.com> for STMicroelectronics.
+ *
+ * Copyright (c) 2020 STMicroelectronics. All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *
+ *     http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ *
+ *
+ * Inspired by:
+ * https://github.com/tensorflow/tensorflow/tree/master/tensorflow/lite/examples/label_image
+ * Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 #ifndef WRAPPER_TFL_H_
 #define WRAPPER_TFL_H_
