29922a51261a08d6c47d67d75e4a5de45f7dba64
Maxence GUILHIN <maxence.guilhin@st.com>
2023-06-21 17:38:50 +0200

    cv-apps : add a specific camera configuration for MP157F-EV1
    Add temporarily a specific configuration of the camera in QVGA at @15fps
    to be able to launch application. With a higher camera resolution the
    frame are not displayed due to DMA issues.
    
    Signed-off-by: Maxence GUILHIN <maxence.guilhin@st.com>

--- recipes-samples/onnxrt-cv-apps/files/resources/config_board.sh
+++ recipes-samples/onnxrt-cv-apps/files/resources/config_board.sh
@@ -3,6 +3,7 @@
 COMPATIBLE=$(cat /proc/device-tree/compatible)
 STM32MP135="stm32mp135"
 STM32MP157="stm32mp157"
+STM32MP157FEV1="stm32mp157f-ev1st"
 
 if [[ "$COMPATIBLE" == *"$STM32MP135"* ]]; then
   MACHINE=$STM32MP135
@@ -10,14 +11,22 @@ if [[ "$COMPATIBLE" == *"$STM32MP135"* ]]; then
   DHEIGHT=240
   DFPS=10
   COMPUTE_ENGINE=""
-  echo "machine used = "$MACHINE
 fi
 
 if [[ "$COMPATIBLE" == *"$STM32MP157"* ]]; then
-  MACHINE=$STM32MP157
-  DWIDTH=640
-  DHEIGHT=480
-  DFPS=15
-  COMPUTE_ENGINE=""
-  echo "machine used = "$MACHINE
+  if [[ "$COMPATIBLE" == *"$STM32MP157FEV1"* ]]; then
+    MACHINE=$STM32MP157FEV1
+    DWIDTH=320
+    DHEIGHT=240
+    DFPS=15
+    COMPUTE_ENGINE=""
+  else
+    MACHINE=$STM32MP157
+    DWIDTH=640
+    DHEIGHT=480
+    DFPS=15
+    COMPUTE_ENGINE=""
+  fi
 fi
+
+echo "machine used = "$MACHINE
