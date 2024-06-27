830b56d22c5fdbf28cbd6e58a78eb5e38b420ef7
Vincent ABRIOU <vincent.abriou@st.com>
2024-06-27 15:42:16 +0200

    applications: introduce new resources directory
    Signed-off-by: Vincent ABRIOU <vincent.abriou@st.com>

---
+++ recipes-samples/resources/application-resources.bb
@@ -0,0 +1,59 @@
+# Copyright (C) 2019, STMicroelectronics - All Rights Reserved
+SUMMARY = "Create package containing all resources need by out of the box applications"
+LICENSE = "SLA0044"
+LIC_FILES_CHKSUM  = "file://resources-files/LICENSE;md5=91fc08c2e8dfcd4229b69819ef52827c"
+
+NO_GENERIC_LICENSE[SLA0044] = "resources-files/LICENSE"
+LICENSE:${PN} = "SLA0044"
+
+SRC_URI = "	file://resources-files "
+
+S = "${WORKDIR}"
+
+do_configure[noexec] = "1"
+do_compile[noexec] = "1"
+
+do_install() {
+	install -d ${D}${prefix}/local/x-linux-ai/resources/
+
+	# install all stai resources
+	install -m 0644 ${S}/resources-files/stai_*.png 				${D}${prefix}/local/x-linux-ai/resources
+
+	# install all configuration scripts
+	install -m 0755 ${S}/resources-files/config_board.sh 			${D}${prefix}/local/x-linux-ai/resources
+	install -m 0755 ${S}/resources-files/check_camera_preview.sh 	${D}${prefix}/local/x-linux-ai/resources
+	install -m 0755 ${S}/resources-files/setup_camera.sh 			${D}${prefix}/local/x-linux-ai/resources
+	install -m 0644 ${S}/resources-files/Default.css 				${D}${prefix}/local/x-linux-ai/resources
+	# install all common resources
+	install -m 0644 ${S}/resources-files/label_*.png 				${D}${prefix}/local/x-linux-ai/resources
+	install -m 0644 ${S}/resources-files/exit_*.png 				${D}${prefix}/local/x-linux-ai/resources
+
+	# install all IC resources
+	install -m 0644 ${S}/resources-files/IC_*.png 					${D}${prefix}/local/x-linux-ai/resources
+
+	# install all OD resources
+	install -m 0644 ${S}/resources-files/OD_*.png 					${D}${prefix}/local/x-linux-ai/resources
+
+	# install all FR resources
+	install -m 0644 ${S}/resources-files/FR_*.png 					${D}${prefix}/local/x-linux-ai/resources
+
+}
+
+do_install:append:stm32mp25common(){
+	# overwrite camera setup script for stm32mp2x
+    install -m 0755 ${S}/resources-files/check_camera_preview_main_isp.sh     ${D}${prefix}/local/x-linux-ai/resources/check_camera_preview.sh
+    install -m 0755 ${S}/resources-files/setup_camera_main_isp.sh             ${D}${prefix}/local/x-linux-ai/resources/setup_camera.sh
+
+	# install all stai resources for OVX logos
+	install -m 0644 ${S}/resources-files/stai_mpu_ovx*.png 				      ${D}${prefix}/local/x-linux-ai/resources
+
+	# install all SEMANTIC SEGMENTATION resources
+	install -m 0644 ${S}/resources-files/SEMSEG_*.png 						  ${D}${prefix}/local/x-linux-ai/resources
+
+	# install all POSE ESTIMATION resources
+	install -m 0644 ${S}/resources-files/PE_*.png 							  ${D}${prefix}/local/x-linux-ai/resources
+}
+
+FILES:${PN} += "${prefix}/local/"
+
+RDEPENDS:${PN} += " bash "
---
+++ recipes-samples/resources/files/resources-files/config_board.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#
+# Copyright (c) 2024 STMicroelectronics.
+# All rights reserved.
+#
+# This software is licensed under terms that can be found in the LICENSE file
+# in the root directory of this software component.
+# If no LICENSE file comes with this software, it is provided AS-IS.
+
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+STM32MP1="stm32mp1"
+STM32MP2="stm32mp2"
+STM32MP135="stm32mp135"
+STM32MP157="stm32mp157"
+STM32MP157FEV1="stm32mp157f-ev1st"
+STM32MP257="stm32mp257"
+STM32MP257FEV1="stm32mp257f-ev1st"
+
+if [[ "$FRAMEWORK" == "nbg" ]]; then
+  NN_EXT=".nb"
+elif [[ "$FRAMEWORK" == "tflite" ]]; then
+  NN_EXT=".tflite"
+elif [[ "$FRAMEWORK" == "onnx" ]]; then
+  NN_EXT=".onnx"
+elif [[ "$FRAMEWORK" == "coral" ]]; then
+  NN_EXT="_edgetpu.tflite"
+else
+  #define a default value if no framework is specified
+  if [[ "$COMPATIBLE" == *"$STM32MP1"* ]]; then
+    NN_EXT=".tflite"
+  else
+    NN_EXT=".nb"
+  fi
+fi
+
+if [[ "$COMPATIBLE" == *"$STM32MP135"* ]]; then
+  MACHINE=$STM32MP135
+  DWIDTH=320
+  DHEIGHT=240
+  DFPS=10
+  COMPUTE_ENGINE=""
+  IMAGE_CLASSIFICATION_MODEL="mobilenet/mobilenet_v1_0.5_128_quant$NN_EXT"
+  IMAGE_CLASSIFICATION_LABEL="mobilenet/labels_imagenet"
+  IMAGE_CLASSIF_DATA="mobilenet/testdata/"
+  OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v1_10_300$NN_EXT"
+  OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset"
+  OBJ_DETECT_DATA="coco_ssd_mobilenet/testdata/"
+fi
+
+if [[ "$COMPATIBLE" == *"$STM32MP157"* ]]; then
+  if [[ "$COMPATIBLE" == *"$STM32MP157FEV1"* ]]; then
+    MACHINE=$STM32MP157FEV1
+    DWIDTH=320
+    DHEIGHT=240
+    DFPS=15
+    COMPUTE_ENGINE=""
+    IMAGE_CLASSIFICATION_MODEL="mobilenet/mobilenet_v1_0.5_128_quant$NN_EXT"
+    IMAGE_CLASSIFICATION_LABEL="mobilenet/labels_imagenet"
+    IMAGE_CLASSIF_DATA="mobilenet/testdata/"
+    OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v1_10_300$NN_EXT"
+    OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset"
+    OBJ_DETECT_DATA="coco_ssd_mobilenet/testdata/"
+  else
+    MACHINE=$STM32MP157
+    DWIDTH=640
+    DHEIGHT=480
+    DFPS=15
+    COMPUTE_ENGINE=""
+    IMAGE_CLASSIFICATION_MODEL="mobilenet/mobilenet_v1_0.5_128_quant$NN_EXT"
+    IMAGE_CLASSIFICATION_LABEL="mobilenet/labels_imagenet"
+    IMAGE_CLASSIF_DATA="mobilenet/testdata/"
+    OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v1_10_300$NN_EXT"
+    OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset"
+    OBJ_DETECT_DATA="coco_ssd_mobilenet/testdata/"
+  fi
+fi
+
+if [[ "$COMPATIBLE" == *"$STM32MP257"* ]]; then
+  SEMANTIC_SEGMENTATION_MODEL="deeplabv3/deeplabv3_257_int8_per_tensor$NN_EXT"
+  SEMANTIC_SEGMENTATION_LABEL="deeplabv3/labels_pascalvoc"
+  SEMANTIC_SEGMENTATION_DATA="deeplabv3/testdata/"
+  POSE_ESTIMATION_DATA="yolov8n_pose/testdata/"
+  POSE_ESTIMATION_MODEL="yolov8n_pose/yolov8n_256_quant_pt_uf_pose_coco-st$NN_EXT"
+  if [[ "$COMPATIBLE" == *"$STM32MP257FEV1"* ]]; then
+    MACHINE=$STM32MP257FEV1
+    DWIDTH=760
+    DHEIGHT=568
+    DFPS=30
+    COMPUTE_ENGINE="--npu"
+    IMAGE_CLASSIFICATION_MODEL="mobilenet/mobilenet_v2_1.0_224_int8_per_tensor$NN_EXT"
+    IMAGE_CLASSIFICATION_LABEL="mobilenet/labels_imagenet_2012"
+    IMAGE_CLASSIF_DATA="mobilenet/testdata/"
+    if [[ "$NN_EXT" == ".nb" ]]; then
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v2_fpnlite_10_256_int8_per_tensor$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset_80"
+    elif [[ "$NN_EXT" == "_edgetpu.tflite" ]]; then
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v1_10_300$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset"
+    else
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v2_fpnlite_10_256_int8$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset_80"
+    fi
+    OBJ_DETECT_DATA="coco_ssd_mobilenet/testdata/"
+    OPTIONS="--dual_camera_pipeline"
+  else
+    MACHINE=$STM32MP257
+    DWIDTH=640
+    DHEIGHT=480
+    DFPS=30
+    COMPUTE_ENGINE="--npu"
+    IMAGE_CLASSIFICATION_MODEL="mobilenet/mobilenet_v2_1.0_224_int8_per_tensor$NN_EXT"
+    IMAGE_CLASSIFICATION_LABEL="mobilenet/labels_imagenet_2012"
+    IMAGE_CLASSIF_DATA="mobilenet/testdata/"
+    if [[ "$NN_EXT" == ".nb" ]]; then
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v2_fpnlite_10_256_int8_per_tensor$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset_80"
+    elif [[ "$NN_EXT" == "_edgetpu.tflite" ]]; then
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v1_10_300$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset"
+    else
+      OBJ_DETEC_MODEL="coco_ssd_mobilenet/ssd_mobilenet_v2_fpnlite_10_256_int8$NN_EXT"
+      OBJ_DETEC_MODEL_LABEL="coco_ssd_mobilenet/labels_coco_dataset_80"
+    fi
+    OBJ_DETECT_DATA="coco_ssd_mobilenet/testdata/"
+    OPTIONS="--dual_camera_pipeline"
+  fi
+fi
+
+echo "machine used = "$MACHINE
